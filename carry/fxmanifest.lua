77d2019a02eb013694ff8ac7cda0cda34fb3fc1e
AgKaLiTSaS <78315323+Billtastic@users.noreply.github.com>
2021-09-28 22:42:20 +0300

    Add files via upload

---
+++ carry/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Squeaky#1289'
+description 'CarryPeople'
+version '1.0.3'
+
+client_script "cl_carry.lua"
+server_script "sv_carry.lua"
